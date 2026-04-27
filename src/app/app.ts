b8f48109ff25bbe27d330868aa03cdd924fdcc07
JeBrandao <jebrandao@firjan.com.br>
2026-04-27 14:43:33 -0300

    Consumo ViaCep

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,54 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CepService } from './services/cep';
+import { Cep } from './models/cep';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ApiViaCep');
+
+  private cepService = inject(CepService);
+
+  cepControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^\d{8}$/)
+  ]);
+
+  endereco = signal<Cep | null>(null);
+  loading = signal(false);
+  erro = signal('');
+
+  buscar() {
+    if (this.cepControl.invalid) {
+      this.erro.set('Digite um CEP válido com 8 números.');
+      return;
+    }
+
+    this.loading.set(true);
+    this.erro.set('');
+    this.endereco.set(null);
+
+    const cep = this.cepControl.value!;
+
+    this.cepService.buscarCep(cep).subscribe({
+      next: (resposta) => {
+        this.loading.set(false);
+
+        if (resposta.erro) {
+          this.erro.set('CEP não encontrado.');
+          return;
+        }
+
+        this.endereco.set(resposta);
+      },
+      error: () => {
+        this.loading.set(false);
+        this.erro.set('Erro ao consultar API.');
+      }
+    });
+  }
 }
